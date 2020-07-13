4002ebbe16c0992649b89c6be3f879d16a74cc60
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-07-13 15:47:33 +0200

    improve comments

--- src/components/RockPaperScissors.js
+++ src/components/RockPaperScissors.js
@@ -11,11 +11,11 @@ const [rock, paper, scissors] = weapons
 export default function RockPaperScissors(props) {
   // 1- 👉👉👉 CREATE SLICES OF APPLICATION STATE (USING TOP-LEVEL COMPONENT STATE)
 
-  // We need several slices of state, to keep track of:
-  //   - the number of rounds played, start at 1
-  //   - the number of 'wins', start at 0
-  //   - the number of 'losses', start at 0
-  //   - the 'message' displayed on the screen, start at 'Fight!'
+  // We need several slices of component state, to keep track of the general state of the app:
+  //   - the number of rounds played, initialize at number 1
+  //   - the number of 'wins', initialize at number 0
+  //   - the number of 'losses', initialize at number 0
+  //   - the 'message' displayed on the screen, initialize at string 'Fight!'
 
   // 2- 👉👉👉 We DO NOT need a slice for rounds tied, as we can calculate that info using other slices
   // 3- 👉👉👉 REPLACE DOWN IN THE JSX THE HARD CODED PIECES OF DATA WITH PIECES OF STATE OR PROPS
@@ -26,41 +26,25 @@ export default function RockPaperScissors(props) {
     // 5- 👉👉👉 IMPLEMENT THE 'play' HANDLER!
 
     // ⛔⛔⛔ DO NOT MUTATE STATE, EVER! PROVIDE A NEW ONE ⛔⛔⛔
-    // ⛔⛔⛔ DO NOT MUTATE STATE, EVER! PROVIDE A NEW ONE ⛔⛔⛔
-    // ⛔⛔⛔ DO NOT MUTATE STATE, EVER! PROVIDE A NEW ONE ⛔⛔⛔
 
     // 5A- Update the round count
 
     // 5B- Pull out which weapon the user selected from the event.target
     // this is the player weapon
 
-    // 5C- this is the computer weapon
+    // 5C- this is the computer weapon (random number either 0 or 1 or 2)
     const computer = weapons[Math.floor(Math.random() * 3)]
 
-    if (
-      /* 5D- it's a LOSS */
-      null
-    ) {
-      // - Update the losses
-      // - Update the message
-    } else if (
-    /* 5E- it's a WIN */
-      null
-    ) {
-      // - Update the wins
-      // - Update the message
-    } /* 5F- it's a TIE */ else {
-      // - Update the message
-    }
+    // 5D- change the state depending on the pair of weapons
   }
 
   return (
     <div className='container'>
-      <div className='player'>Welcome, {player}</div>
-      {/* Replace hard-coded round */}
+      {/* Replace hard-coded round using props */}
+      <div className='player'>Welcome, Ava</div>
+      {/* Replace hard-coded round using state */}
       <h2>Round: 1</h2>
-
-      {/* Replace hard-coded stats */}
+      {/* Replace hard-coded stats using state */}
       <h3>won: 0 | lost: 0 | tied: 0</h3>
       <h1 className='message'>Fight!</h1>
 
@@ -76,4 +60,4 @@ export default function RockPaperScissors(props) {
   )
 }
 
-// STRETCH- 👉👉👉 ADD A RESTART BUTTON, ANYTHING YOU CAN IMAGINE, BRING OTHER COMPONENTS...
+// STRETCH- 👉👉👉 ADD A RESTART BUTTON, BREAK INTO SUBCOMPONENTS...
