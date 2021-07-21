421933a6a35e4f7edb70356ca2e122ec76c5cfa2
Timothy Roy <timothy.roy@protonmail.ch>
2021-07-20 22:47:37 -0500

    Create and use React components, the powerrrr

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -8,3 +8,41 @@
   - We never tamper with state: `healthPoints++`, `healthPoints--` or `someState.push(item)` is FORBIDDEN
   - We use the dedicated "state updater" to schedule a state change: `setHealthPoints(healthPoints + 1)`
 */
+
+// What is a 'hook'? 
+// we use hooks because React hooks into them - runs again when the value changes
+
+
+// React has class components
+//// in old days, had to know the component lifecycle
+////// creation, updating
+// Hooks allow functional components to have state as well
+
+import React, { useState } from 'react' // hey React, come translate this file!
+
+export function Playground(props) {
+  const [value, setValue] = useState('Hi there')
+  const [count, setCount] = useState(0)
+  const [dogs, setDogs] = useState([])
+
+  // axios
+  // .get('mydogsUrl')
+  // .then(dogsData => {
+  //   setDogs(dogsData)
+  // })
+
+  // return something
+  return (
+    <>
+     <p>Hello world!</p>
+     <p>{`Is the playground rainy? ${props.rainy}`}</p>
+     <p>Current count is: {count}</p>
+     <p>Current value is: {value}</p>
+     <button onClick={() => setCount(count + 1)}>Count!</button>
+     <button onClick={() => setValue('howdy')}>Click the button!</button>
+   </>
+  )
+}
+
+// Swingset, to be used inside Playground
+// Is the swingset wet?
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import { render } from 'react-dom'
+// import { render } from 'react-dom'
+import ReactDOM from 'react-dom'
+
+import { Playground } from './components/Playground'
 
 /*
 💥💥💥 Rules when DECLARING a React component 💥💥💥
@@ -10,7 +13,7 @@ import { render } from 'react-dom'
   - Attribute names with dashes are camelCased (except data- & aria-)
   - The `class` attribute is `className`, `for` attribute is `htmlFor`
   - We can INTERPOLATE 🔥 js expressions 🔥 using curly brackets
-  - We interpolate attribute values and content
+  - We interpolate attribute values and content -- LOGIC AND HTML LIVE TOGETHER IN HARMONY
 */
 
 /*
@@ -21,15 +24,46 @@ import { render } from 'react-dom'
   - Don't forget all tags need to close correctly
 */
 
+
+// state: all info passed into a component - or your entire app!
+// you can make a component track info by passing it in as props
+// giving state to a component
+
+// const myDogsData = [{}]
+// axios.get()
+
+
 function App(props) {
+  const { cohort, dayofweek, currentYear, sunny, rainy } = props // destructuring is never mandatory!
+
+  console.log(props)
+  console.log(cohort, dayofweek, currentYear, sunny, rainy)
+
+  const myBrandNewProp = 42
+
+  const isEven = Math.floor(Math.random() * 10) % 2 === 0 // true if even, false if odd
+
   return (
     <div className='container'>
-      <h1>Welcome to React, Web {props.cohort}</h1>
+      <h1>Welcome to React, Web {cohort}</h1>
+      <p>The day of the week is: {dayofweek}</p>
+      <p>The current year is: {currentYear}</p>
+      <p>Is today sunny? {isEven ? 'yes' : 'no'}</p>
+      <p>Is today rainy? {rainy ? 'yes' : 'no'}</p>
+      <p>{`Is it rainy? ${rainy}`}</p>
+
+      <Playground rainy={rainy} meaning={myBrandNewProp} />
+
     </div>
   )
 }
+// { rainy: false }
 
-render(
-  <App cohort='37' />,
+// const myProps = { cohort: '37', dayofweek: 'Tuesday', currentYear: '2021', sunny: true }
+// App(myProps)
+
+
+ReactDOM.render(
+  <App cohort='31' dayofweek='Tuesday' currentYear='2021' rainy={true} />,
   document.querySelector('#root')
 )
