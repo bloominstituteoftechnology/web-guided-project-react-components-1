3f79a3800073d16943fc098c2e33583a7ba00984
CRHarding <34920615+CRHarding@users.noreply.github.com>
2021-08-09 11:08:51 -0400

    Update Playground.js

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -2,7 +2,7 @@
 💥💥💥 Rules of STATE 💥💥💥
   - We create a slice of state like this: `const [healthPoints, setHealthPoints] = useState(100)`
   - A component may have as may slices of state as it needs
-  - An slice of state may contain a string, a number, a boolean, an array, an object...
+  - A slice of state may contain a string, a number, a boolean, an array, an object...
   - We use slices of state to store information that changes as the user interacts with the app
   - State is used in the JSX interpolated inside curly brackets
   - We never tamper with state: `healthPoints++`, `healthPoints--` or `someState.push(item)` is FORBIDDEN
